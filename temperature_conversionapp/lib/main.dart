e110e06859fbf595da0d9ec8d9a839270b6a458b
Stecie06 <n.stecie@alustudent.com>
2025-05-30 19:09:18 +0200

    ConversionApp

--- temperature_conversionapp/lib/main.dart
+++ temperature_conversionapp/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(TemperatureConverterApp());
+  runApp(TemperatureConverterAp());
 }
 
 class TemperatureConverterApp extends StatelessWidget {
